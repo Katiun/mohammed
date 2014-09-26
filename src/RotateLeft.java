fafb60cdfff75f3959799ce2899d5bc78e0e966b
Henry <katiun7@hotmail.com>
2014-09-26 20:19:01 -0300

    Rotate Left
    Rota 270 en sentido contrario, en vez de 90

--- src/RotateLeft.java
+++ src/RotateLeft.java
@@ -1,3 +1,4 @@
+import lejos.nxt.Motor;
 import lejos.nxt.addon.CompassHTSensor;
 import lejos.robotics.navigation.CompassPilot;
 import lejos.robotics.subsumption.Behavior;
@@ -7,6 +8,8 @@ public class RotateLeft implements Behavior{
 
 	CompassPilot pilot;
 	CompassHTSensor cs;
+	private final int SPEED = 200;
+	private final int ANGLE = 200;
 	
 	public RotateLeft(CompassPilot pilot, CompassHTSensor cs){
 		this.pilot = pilot;
@@ -26,14 +29,19 @@ public class RotateLeft implements Behavior{
 		Variables.turn = false;
 		//positivo a la izquierda, negativo a la derecha
 		pilot.resetCartesianZero();
-//		float degrees = cs.getDegreesCartesian();
-//		while (degrees != 90){
-//			System.out.println("" + degrees);
-//			degrees = cs.getDegreesCartesian();
-//		}
-		pilot.rotate(90);
-//		pilot.stop();
-		System.out.println("ACT FIN");
+////		float degrees = cs.getDegreesCartesian();
+////		while (degrees != 90){
+////			System.out.println("" + degrees);
+////			degrees = cs.getDegreesCartesian();
+////		}
+//		pilot.rotate(90);
+////		pilot.stop();
+//		System.out.println("ACT FIN");
+		Motor.C.setSpeed(SPEED);
+		Motor.C.rotateTo(ANGLE);
+		pilot.rotate(-270);
+		Motor.C.rotateTo(0);
+		Motor.C.stop();		
 	}
 
 	@Override
